938b4dc22a0c2456c7293443ce5c5945123a28c1
Volodymyr Yahello <vyahello@gmail.com>
2018-08-15 23:11:19 +0300

    Add snake food

---
+++ snake/food.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from random import randint
+from snake.window import Window
+
+
+class Food(ABC):
+    """Abstract interface for some food."""
+
+    @abstractmethod
+    def longitude(self) -> int:
+        pass
+
+    @abstractmethod
+    def latitude(self) -> int:
+        pass
+
+    @abstractmethod
+    def render(self) -> None:
+        pass
+
+    @abstractmethod
+    def reset(self) -> None:
+        pass
+
+
+class SnakeFood(Food):
+    """Snake food interface."""
+
+    def __init__(self, window: Window, max_long: int, max_lat: int, entity: str = 'x') -> None:
+        self._longitude: int = randint(1, max_long)
+        self._latitude: int = randint(1, max_lat)
+        self._max_long: int = max_long
+        self._max_lat: int = max_lat
+        self._entity: str = entity
+        self._window = window
+
+    @property
+    def longitude(self) -> int:
+        return self._longitude
+
+    @property
+    def latitude(self) -> int:
+        return self._latitude
+
+    def render(self) -> None:
+        self._window.add_str(self._latitude, self._longitude, self._entity)
+
+    def reset(self) -> None:
+        self._longitude = randint(1, self._max_long)
+        self._latitude = randint(1, self._max_lat)
