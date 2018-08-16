5bf5bf3ac686a2198d7afe5eb1b406a9e47e2253
Volodymyr Yahello <vyahello@gmail.com>
2018-08-16 16:13:56 +0300

    Cover terminal attributes with unittests

---
+++ tests/environment/test_attributes.py
@@ -0,0 +1,17 @@
+from lib.environment import WIDTH, HEIGHT, MAX_LONGITUDE, MAX_LATITUDE
+
+
+def test_env_width() -> None:
+    assert WIDTH == 60
+
+
+def test_env_height() -> None:
+    assert HEIGHT == 38
+
+
+def test_env_mx_long() -> None:
+    assert MAX_LONGITUDE == 58
+
+
+def test_env_mx_lat() -> None:
+    assert MAX_LATITUDE == 36
